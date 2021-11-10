fef5d2dbd7bfb6ab5b4c9cf7551df52571fc2d69
FatherMonkey916 <prupexekifre-6517@imails.asso.st>
2021-11-10 16:39:57 +0900

    Install react-hook-form and impliment basic validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import Form from "./Form";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Learn to code by watching others</h1>
+
+      <p>
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers this is
+        invaluable.
+      </p>
+
+      <div className="App-blue-box">
+        <p>Try it free 7 days then $20/mo. thereafter.</p>
+      </div>
+
+      <Form />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Form.tsx
@@ -0,0 +1,80 @@
+import { useForm } from "react-hook-form";
+import "./Form.css";
+
+type UserInfo = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+};
+
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<UserInfo>();
+  const onSubmit = (data: UserInfo) => {
+    console.log("data", data);
+  };
+
+  return (
+    <div className="Form">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="field">
+          <input
+            type="text"
+            id="firstName"
+            placeholder="First Name"
+            {...register("firstName", { required: true })}
+            autoFocus
+          />
+          {errors.firstName && <p>First Name cannot be empty</p>}
+        </div>
+
+        <div className="field">
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Last Name"
+            {...register("lastName", { required: true })}
+          />
+          {errors.lastName && <p>Last Name cannot be empty</p>}
+        </div>
+
+        <div className="field">
+          <input
+            type="email"
+            id="email"
+            placeholder="Email Address"
+            {...register("email", {
+              required: true,
+              pattern: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+            })}
+          />
+          {errors?.email?.type == "required" && <p>Email cannot be empty</p>}
+          {errors?.email?.type == "pattern" && (
+            <p>Looks like this is not an email</p>
+          )}
+        </div>
+
+        <div className="field">
+          <input
+            type="password"
+            id="pwd"
+            placeholder="Password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && <p>Password cannot be empty</p>}
+        </div>
+
+        <button type="submit">Claim Your Free Trial</button>
+      </form>
+      <p>
+        By clicking the button, you are agreeing to our{" "}
+        <a href="#">Terms and Services</a>
+      </p>
+    </div>
+  );
+}
